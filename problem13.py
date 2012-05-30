6eddd447c53aba7516274246b7230f387b70f914
toshi <me@toshimaru.net>
2012-05-31 03:31:45 +0900

    add a solution

---
+++ problem13.py
@@ -0,0 +1,9 @@
+﻿# -*- coding: utf-8 -*-
+
+# sum([sumしたい配列])
+print "answer:" , str(sum([int(x) for x in open("euler13.txt")]))
+
+#for x in open("euler13.txt"):
+#	print int(x)
+#	sum += int(x)
+#	print sum
