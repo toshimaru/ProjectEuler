a1d01c6bb72ff098d245f13554dcda397b8af56e
toshi <me@toshimaru.net>
2013-07-18 23:55:40 -0700

    add problem21 solution

--- problem20.rb
+++ problem20.rb
@@ -2,4 +2,4 @@ num = 1
 (2..100).each { |i|
   num *= i
 }
-puts num.to_s().split('').inject { |test, i| test.to_i() + i.to_i() }
+puts num.to_s().split('').inject { |sum, i| sum.to_i() + i.to_i() }
